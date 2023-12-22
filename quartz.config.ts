49784babf422008b1f307fb685d91195a9eb0372
mancuoj <1337381768@qq.com>
2023-12-22 10:12:22 +0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "mancuoj.me",
+    pageTitle: "Cybergarden",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
