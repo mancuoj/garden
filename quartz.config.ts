3eafc082d59f69e4bcf8797b4b2bab05b8be1fe4
mancuoj <1337381768@qq.com>
2024-01-04 21:41:14 +0800

    vault backup: 2024-01-04 21:41:14
    Affected files:
    content/.obsidian/workspace.json
    content/POSTS/Angular ngDoCheck.md
    content/POSTS/ChatGPT 中英翻译 Prompt.md
    content/POSTS/中文技术文档排版指南.md
    content/TIL/002.md
    quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -5,7 +5,7 @@ const config: QuartzConfig = {
   configuration: {
     pageTitle: "Cybergarden",
     enableSPA: true,
-    enablePopovers: true,
+    enablePopovers: false,
     analytics: {
       provider: "plausible",
     },
