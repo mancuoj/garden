b34f73570d4d01fc8315d38e3a6a19ef001a6c92
mancuoj <mancuoj@gmail.com>
2024-02-28 01:23:07 +0800

    fmt

--- content/.obsidian/plugins/obsidian-git/main.js
+++ content/.obsidian/plugins/obsidian-git/main.js
@@ -3081,9 +3081,8 @@ var require_trees = __commonJS({
       }
       node = elems
       do {
-        n =
-          s.heap[1]
-          /*SMALLEST*/
+        n = s.heap[1]
+        /*SMALLEST*/
         s.heap[1] = s.heap[s.heap_len--]
         /*SMALLEST*/
         pqdownheap(
@@ -3092,16 +3091,15 @@ var require_trees = __commonJS({
           1,
           /*SMALLEST*/
         )
-        m =
-          s.heap[1]
-          /*SMALLEST*/
+        m = s.heap[1]
+        /*SMALLEST*/
         s.heap[--s.heap_max] = n
         s.heap[--s.heap_max] = m
         tree[node * 2] = tree[n * 2] + tree[m * 2]
         s.depth[node] = (s.depth[n] >= s.depth[m] ? s.depth[n] : s.depth[m]) + 1
         tree[n * 2 + 1] = tree[m * 2 + 1] = node
         s.heap[1] =
-        /*SMALLEST*/
+          /*SMALLEST*/
           node++
         pqdownheap(
           s,
@@ -3110,9 +3108,8 @@ var require_trees = __commonJS({
           /*SMALLEST*/
         )
       } while (s.heap_len >= 2)
-      s.heap[--s.heap_max] =
-        s.heap[1]
-        /*SMALLEST*/
+      s.heap[--s.heap_max] = s.heap[1]
+      /*SMALLEST*/
       gen_bitlen(s, desc)
       gen_codes(tree, max_code, s.bl_count)
     }
