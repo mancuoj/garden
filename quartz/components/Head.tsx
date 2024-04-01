b2ad50953c71c26895a8d4f05200d501bb08a0ca
mancuoj <mancuoj@gmail.com>
2024-04-01 20:37:37 +0800

    vault backup: 2024-04-01 20:37:37
    Affected files:
    package-lock.json
    package.json
    pnpm-lock.yaml
    quartz/build.ts
    quartz/cfg.ts
    quartz/components/ContentMeta.tsx
    quartz/components/Footer.tsx
    quartz/components/Graph.tsx
    quartz/components/Head.tsx
    quartz/components/PageList.tsx
    quartz/components/RecentNotes.tsx
    quartz/components/TagList.tsx
    quartz/components/pages/FolderContent.tsx
    quartz/components/pages/TagContent.tsx
    quartz/components/renderPage.tsx
    quartz/components/scripts/graph.inline.ts
    quartz/components/scripts/search.inline.ts
    quartz/components/styles/contentMeta.scss
    quartz/i18n/index.ts
    quartz/i18n/locales/fr-FR.ts
    quartz/i18n/locales/hu-HU.ts
    quartz/i18n/locales/pt-BR.ts
    quartz/plugins/emitters/componentResources.ts
    quartz/plugins/emitters/tagPage.tsx
    quartz/plugins/index.ts
    quartz/plugins/transformers/citations.ts
    quartz/plugins/transformers/description.ts
    quartz/plugins/transformers/frontmatter.ts
    quartz/plugins/transformers/index.ts
    quartz/plugins/transformers/ofm.ts
    quartz/static/og-image.png
    quartz/styles/base.scss
    quartz/util/path.ts
    quartz/util/theme.ts

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -1,6 +1,7 @@
 import { i18n } from "../i18n"
 import { FullSlug, joinSegments, pathToRoot } from "../util/path"
 import { JSResourceToScriptElement } from "../util/resources"
+import { googleFontHref } from "../util/theme"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 export default (() => {
@@ -15,16 +16,17 @@ export default (() => {
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
 
     const iconPath = joinSegments(baseDir, "static/icon.png")
-    const ogImagePath = `https://${cfg.baseUrl}/static/icon.png`
+    const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
       <head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        {cfg.theme.cdnCaching && (
+        {cfg.theme.cdnCaching && cfg.theme.fontOrigin === "googleFonts" && (
           <>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link rel="stylesheet" href={googleFontHref(cfg.theme)} />
           </>
         )}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
